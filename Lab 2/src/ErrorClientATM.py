e7c846149f5534e19f97f5c0d51c9f4118207e8b
Mahmudul Hasan <96936868+saged-sama@users.noreply.github.com>
2024-02-03 11:52:17 +0600

    Add files via upload

---
+++ Lab 2/src/ErrorClientATM.py
@@ -0,0 +1,43 @@
+import socket
+import time
+
+DEST_IP = socket.gethostbyname(socket.gethostname())
+
+# DEST_IP = '10.33.2.94'
+DEST_PORT = 12348
+ENCODER = "utf-8"
+BYTESIZE = 1024
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((DEST_IP, DEST_PORT))
+
+amount = "0"
+opcode = "dp"
+
+start_time = time.time()
+end_time = time.time()
+
+requested = False
+
+while True:
+    message = client_socket.recv(BYTESIZE).decode(ENCODER)
+    if message.lower() == "quit":
+        client_socket.send("quit".encode(ENCODER))
+        print("\nEnding the server... goodbye!")
+        break
+    else:
+        print(f"\nServer Response:\n{message}")
+
+        if requested:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Elapsed: {elapsed_time}")
+        
+        amount = input("\nAmount: ")
+        client_socket.send(amount.encode(ENCODER))
+        opcode = input("Type in wd or dp for withdrawal or deposit: ")
+        client_socket.send(opcode.encode(ENCODER))
+        requested = True
+        start_time = time.time()
+
+client_socket.close()
---
+++ Lab 2/src/ErrorServerATM.py
@@ -0,0 +1,54 @@
+import socket
+import random
+import time
+
+# HOST_IP = '10.33.2.94'
+HOST_IP = socket.gethostbyname(socket.gethostname())
+HOST_PORT = 12348
+ENCODER = "utf-8"
+BYTESIZE = 1024
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST_IP, HOST_PORT))
+server_socket.listen()
+
+print("Server is running... \n")
+client_socket, client_address = server_socket.accept()
+
+client_socket.send("You are connected to the server...".encode(ENCODER))
+
+errorPercentage = 0.5
+balance = 50000 #or whatever 
+
+while True:
+    error = random.random()
+    print("Error: ", error, "\n")
+
+    if error <= errorPercentage:
+        print('Error has occurred!! Waiting for client...')
+    else:
+        amount = client_socket.recv(BYTESIZE).decode(ENCODER)
+        print('\nClient Request Received:\n')
+        print(f'Amount: {amount}')
+        op = client_socket.recv(BYTESIZE).decode(ENCODER)
+        print(f'Requested operation: {op}\n')
+        if op == 'quit':
+            client_socket.send('quit'.encode(ENCODER))
+            print("\nEnding the server... goodbye!")
+            break
+        elif op == 'wd':
+            if balance < int(amount):
+                client_socket.send("You have insufficient funds!!".encode(ENCODER))
+                print('Insufficient fund responded\n')
+            else:
+                balance -= int(amount)
+                response = "Amount withdrawn: " + str(amount) + "\nBalance: " + str(balance)
+                client_socket.send(response.encode(ENCODER))
+                print('Successful withdrawal responded\n')
+        elif op == 'dp':
+            balance += int(amount)
+            response = "Amount diposited: " + str(amount) + "\nBalance: " + str(balance)
+            client_socket.send(response.encode(ENCODER))
+            print('Successful diposition responded\n')
+
+server_socket.close()

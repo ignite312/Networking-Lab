07e4db67ad701f46021bdc4c554f5130c43bcb89
saged-sama <mahmudulhasanshajid@gmail.com>
2024-02-21 23:20:41 +0600

    Lab 5

--- Lab 5/Task1/Client/Client.py
+++ Lab 5/Task1/Client/Client.py
@@ -1,22 +1,40 @@
 import socket
-import random
+import struct
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = "192.168.0.127"
 PORT = 12345
 client_socket.connect((HOST, PORT))
-acknowledgement = client_socket.recv(1024)
-print('Acknowledgement: ', acknowledgement.decode(), '\n')
 
-filepath = input('Enter the filepath: ')
-client_socket.send(filepath.encode()) 
+def send_data(data):
+    sequence_number = 0
 
-with open(filepath, 'wb') as file:
     while True:
-        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, random.randint(1024, 4096))
-        data = client_socket.recv(256)
-        if not data:
+        packet = data[sequence_number]
+        client_socket.sendall(struct.pack("!I", sequence_number))
+        client_socket.send(packet)
+
+        acknowledgment_number, = struct.unpack("!I", client_socket.recv(4))
+        print(f'Received acknowledged sequence number: {acknowledgment_number}')
+
+        if acknowledgment_number == len(data):
+            client_socket.sendall(struct.pack("!I", 0))
+            packet = b""
+            client_socket.sendall(packet)
             break
-        file.write(data)
+
+        sequence_number = acknowledgment_number
+
+window_size, = struct.unpack("!I", client_socket.recv(4))
+print(f'Server specified window size: {window_size}\n')
+
+data = []
+with open("something.pdf", "rb") as file:
+    buff = file.read(window_size)
+    while len(buff):
+        data.append(buff)
+        buff = file.read(window_size)
+
+send_data(data)
 
 client_socket.close()
